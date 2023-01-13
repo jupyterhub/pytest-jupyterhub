beb9c77a7fb9886e95066f63d7f3c59fe53c125b
Sheila Kahwai <sheilankw@gmail.com>
2023-01-13 11:35:38 +0300

    create initial plugin module

---
+++ pytest_jupyterhub/jupyterhub_spawners.py
@@ -0,0 +1,10 @@
+""" 
+This plugin module will create a lightweight jupyterhub application that
+can be used to test different spawner implementations
+"""
+
+from pytest import fixture
+
+@fixture
+def app_for_spawners():
+    pass
