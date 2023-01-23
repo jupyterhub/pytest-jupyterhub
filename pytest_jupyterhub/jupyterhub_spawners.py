572e473d75ca5fe25d2081dc116aa2cb0d4fb345
Sheila Kahwai <sheilankw@gmail.com>
2023-01-23 16:22:00 +0300

    create fixture to return base Spawner class

--- pytest_jupyterhub/jupyterhub_spawners.py
+++ pytest_jupyterhub/jupyterhub_spawners.py
@@ -3,29 +3,11 @@ This plugin module will create a lightweight jupyterhub application that
 can be used to test different spawner implementations
 """
 
-import asyncio
-import sys
-
 import pytest
-from jupyterhub.jupyterhub.mocking import MockHub
+from jupyterhub.spawner import Spawner
 
 
 @pytest.fixture
-def app_for_spawners(request):
-    mocked_app = MockHub()
-
-    async def make_app():
-        await mocked_app.initialize([])
-        await mocked_app.start()
-
-    def fin():
-        mocked_app.log.handlers = []
-        MockHub.clear_instance()
-        try:
-            mocked_app.stop()
-        except Exception as e:
-            print("Error stopping Hub: %s" % e, file=sys.stderr)
-
-    request.addfinalizer(fin)
-    asyncio.run(make_app())
-    return mocked_app
+def spawner_class():
+    # returns the spawner class to be used by the hub
+    return Spawner
