5045949e085bd0ad9cf630621cabd131e638da55
Sheila Kahwai <sheilankw@gmail.com>
2023-01-24 11:01:34 +0300

    add fixture configuring JupyterHub to use the Spawner class

--- pytest_jupyterhub/jupyterhub_spawners.py
+++ pytest_jupyterhub/jupyterhub_spawners.py
@@ -5,9 +5,18 @@ can be used to test different spawner implementations
 
 import pytest
 from jupyterhub.spawner import Spawner
+from traitlets.config import Config
 
 
 @pytest.fixture
 def spawner_class():
     """Allows tests to setup the Spawner class that will be used by the hub."""
     return Spawner
+
+
+@pytest.fixture
+def jupyterhub_spawner_config(spawner_class):
+    """Configures JupyterHub to use the Spawner class"""
+    c = Config()
+    c.JupyterHub.spawner_class = spawner_class
+    return c
