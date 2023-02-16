3570cced314aa616ddf4bd99d698dbca4105c3f8
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2023-02-16 15:25:02 +0200

    Refactor fixtures and tests

--- pytest_jupyterhub/jupyterhub_spawners.py
+++ pytest_jupyterhub/jupyterhub_spawners.py
@@ -6,41 +6,40 @@ can be used to test different spawner implementations
 import sys
 
 import pytest
-from jupyterhub.tests.mocking import MockHub
+from jupyterhub.tests.mocking import MockHub, MockSpawner
 from traitlets.config import Config
 
 
 @pytest.fixture
-async def jupyterhub_spawner_class_config(spawner_class):
-    """Configures JupyterHub to use the Spawner class specified in the spawner_class fixture."""
-    spawner_class_config = {"JupyterHub": {"spawner_class": spawner_class}}
-    return Config(spawner_class_config)
-
-
-@pytest.fixture
-async def spawner_config():
-    """Allows tests to setup configurations that are specific to the spawner implementation"""
-    return Config()
-
-
-@pytest.fixture
-async def app_for_spawners(request, jupyterhub_spawner_class_config, spawner_config):
-    """Creates an instance of a JupyterHub application that can be used by different spawner implementation test suites"""
-    final_config = jupyterhub_spawner_class_config
-    final_config.merge(spawner_config)
-    mocked_app = MockHub.instance(config=final_config)
-
-    def fin():
-        """Disconnects logging during cleanup because pytest closes captured FDs prematurely"""
-        mocked_app.log.handlers = []
+async def get_configured_mockhub_instance():
+    def _create_configured_mockhub_instance(
+        config=Config(),
+    ):
         MockHub.clear_instance()
-        try:
-            mocked_app.stop()
-        except Exception as e:
-            print("Error stopping Hub: %s" % e, file=sys.stderr)
+        mocked_app = MockHub.instance(config=config)
+        return mocked_app
 
-    request.addfinalizer(fin)
-    await mocked_app.initialize([])
-    await mocked_app.start()
+    return _create_configured_mockhub_instance
 
-    return mocked_app
+
+@pytest.fixture
+async def hub_app(hub_app_cleanup):
+    app = MockHub.instance()
+    await app.initialize([])
+    await app.start()
+
+    return app
+
+
+@pytest.fixture()
+def hub_app_cleanup():
+    """Automatically cleans up hub resources."""
+    yield
+    app = MockHub.instance()
+    app.log.handlers = []
+    try:
+        app.stop()
+    except Exception as e:
+        print("Error stopping Hub: %s" % e, file=sys.stderr)
+
+    MockHub.clear_instance()
--- tests/test_jupyterhub_spawners.py
+++ tests/test_jupyterhub_spawners.py
@@ -1,23 +1,37 @@
+from traitlets.config import Config
+
 from jupyterhub.app import JupyterHub
 from jupyterhub.spawner import Spawner
+from jupyterhub.tests.mocking import MockSpawner
+
+async def test_default_mockhub_instance_creator(get_configured_mockhub_instance):
+    hub_instance = get_configured_mockhub_instance()
+    assert isinstance(hub_instance, JupyterHub)
+    assert hub_instance.spawner_class == MockSpawner
+    assert hub_instance.config == {}
 
 
-async def test_spawner_class(spawner_class):
-    """Tests that the spawner_class fixture returns class Spawner"""
-    assert spawner_class == Spawner
+async def test_default_hub_app(hub_app):
+    assert isinstance(hub_app, JupyterHub)
+    assert hub_app.spawner_class == MockSpawner
 
 
-async def test_spawner_config(spawner_config):
-    """Tests that the spawner_config fixture returns an empty config"""
-    assert spawner_config == {}
+async def test_mockhub_instance_creator(get_configured_mockhub_instance):
+    config_dict = {
+        "JupyterHub": {"spawner_class": Spawner},
+        "Spawner": {"home_dir_template": "/tmp/Jeff"},
+    }
+    new_hub_instance = get_configured_mockhub_instance(config=Config(config_dict))
+    assert isinstance(new_hub_instance, JupyterHub)
+    assert new_hub_instance.spawner_class == config_dict["JupyterHub"]["spawner_class"]
+    assert new_hub_instance.config == config_dict
 
 
-async def test_jupyterhub_spawner_class_config(jupyterhub_spawner_class_config):
-    """Tests that the jupyterhub_spawner_class_config returns the class returned by spawner_class fixture"""
-    assert jupyterhub_spawner_class_config["JupyterHub"]["spawner_class"] == Spawner
-
-
-async def test_app_for_spawners(app_for_spawners):
-    """Tests the app_for_spawners fixture"""
-    assert isinstance(app_for_spawners, JupyterHub)
-    assert app_for_spawners.spawner_class == Spawner
+async def test_hub_app(hub_app):
+    assert isinstance(hub_app, JupyterHub)
+    config_dict = {
+        "JupyterHub": {"spawner_class": Spawner},
+        "Spawner": {"home_dir_template": "/tmp/Jeff"},
+    }
+    assert hub_app.spawner_class == config_dict["JupyterHub"]["spawner_class"]
+    assert hub_app.config == config_dict
