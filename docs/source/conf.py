5b7c6ce671cb268aa719eb34dec5b9562cd40406
Sheila Kahwai <sheilankw@gmail.com>
2023-03-23 11:56:22 +0300

    enable colon fence to denote directives

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,15 @@ root_doc = master_doc = "index"
 # The suffix(es) of source filenames.
 source_suffix = [".md"]
 
+# -- MyST configuration ------------------------------------------------------
+# ref: https://myst-parser.readthedocs.io/en/latest/configuration.html
+#
+
+myst_enable_extensions = [
+    # available extensions: https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+    "colon_fence",
+]
+
 # -- Options for HTML output -------------------------------------------------
 # ref: https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
